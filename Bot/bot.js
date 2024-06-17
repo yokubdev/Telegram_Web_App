25d59adf828841ebb8f50d4705f9d3184700e20a
Yokubjon Sobirjonov <yoqub.sobirjonov@upay.uz>
2024-06-17 15:54:56 +0500

    initial

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
+const TOKEN = "7486196695:AAFtKgfen5A79OFMm2X40XDArpIbWgoxWIU";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://celebrated-torte-184681.netlify.app/";
